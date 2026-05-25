ae40052798d2290c214de05b17fe04f8fee643f8
Helvio Junior <helvio_junior@hotmail.com>
2026-05-25 09:09:31 -0300

    v0.1.38

--- turbosearch/__meta__.py
+++ turbosearch/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.37'
+__version__ = '0.1.38'
 __title__ = "turbosearch"
 __description__ = "The most powerfull Automated url finder/crawler."
 __url__ = "https://github.com/helviojunior/turbosearch"
